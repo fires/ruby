791d9c17a1b04a9b5b8965c5f1d2a6584e19adcd
fires <firesaijj@qq.com>
2014-07-24 15:22:59 +0800

    1
    1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,5 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   puts "test git"
+  puts "remote change for upate"
 end
